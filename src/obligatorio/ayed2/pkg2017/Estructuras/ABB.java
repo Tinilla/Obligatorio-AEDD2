33bf68663f73d35e6644d39d251b6cfbd878a4d2
Tinilla <>
2017-11-12 23:44:04 -0300

    Merge origin/Hola

---
+++ src/obligatorio/ayed2/pkg2017/Estructuras/ABB.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package obligatorio.ayed2.pkg2017.Estructuras;
+
+/**
+ *
+ * @author Equipo
+ */
+public class ABB {
+    //Pedo
+}
